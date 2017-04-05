6a7d3e6400a6eafbf396ac3008e1a7d4442ecbd0
SidHarder <softwarenavigator@gmail.com>
2017-04-05 10:45:31 -0600

    adding mysql secret

--- src/encode-secret.js
+++ src/encode-secret.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 const Base64 = require('js-base64').Base64
-const config = require('./mssql-config.js')
+const path = require('path')
+
+const config = require(path.resolve('./secrets/mysql-config/mysql-config.js'))
 
 var json64 = Base64.encode(JSON.stringify(config))
 
-fs.writeFile('./etc/secrets/mssql-config', json64, (err) => {
+fs.writeFile(path.resolve('./secrets/mysql-config/mysql-config'), json64, (err) => {
   if (err) throw err
-  console.log('It\'s saved!')
+  console.log('The secret has been encoded.')
 })
