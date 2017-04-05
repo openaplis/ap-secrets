8de9d136d017fe80fdf07484684746a7bc33f5f7
SidHarder <softwarenavigator@gmail.com>
2017-04-05 11:23:34 -0600

    renaming files

--- src/encodeSecret.js
+++ src/encodeSecret.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const Base64 = require('js-base64').Base64
 const path = require('path')
 
-const config = require(path.resolve('./secrets/mysql-config/mysql-config.js'))
+const config = require(path.resolve('./secrets/mysql-config/mysqlConfig.js'))
 
 var json64 = Base64.encode(JSON.stringify(config))
 
-fs.writeFile(path.resolve('./secrets/mysql-config/mysql-config'), json64, (err) => {
+fs.writeFile(path.resolve('./secrets/mysql-config/mysqlConfig'), json64, (err) => {
   if (err) throw err
   console.log('The secret has been encoded.')
 })
